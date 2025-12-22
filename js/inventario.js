3f59b2ba2e52a8f3f05491df76dba10ca8c9c412
cervantesAxel <axelpartida584@gmail.com>
2025-12-21 23:13:16 -0600

    Modificando rutas

--- js/inventario.js
+++ js/inventario.js
@@ -1,7 +1,7 @@
 import { getUserProfile } from './session.js';
 import { notifySuccess, notifyError } from './utils.js';
 
-const BASE_API_URL = '/api';
+const BASE_API_URL = 'http://localhost:8081/api';
 
 let products = [];
 let inventoryDetailsMap = new Map();
